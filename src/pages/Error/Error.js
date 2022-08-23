62c113f20d78ac832296a491c183746e4992a9af
sourav73 <mdsehab1@gmail.com>
2022-08-23 12:43:39 +0600

    Added some styles in error page

--- src/pages/Error/Error.js
+++ src/pages/Error/Error.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
-  return <div>Error</div>;
+  const attributionStyle = {
+    textDecoration: 'none',
+    color: '#ccc',
+    textAlign: 'center',
+  };
+  return (
+    <Container
+      className="max-width d-flex align-items-center justify-content-center position-relative flex-column"
+      style={{ height: '90vh' }}
+    >
+      <img src="./images/error.jpg" alt="Error" />
+      <a
+        href="https://www.freepik.com/vectors/404-page"
+        style={attributionStyle}
+      >
+        404 page vector created by pikisuperstar - www.freepik.com
+      </a>
+      <Link to="/" className="mt-3">
+        Go Home
+      </Link>
+    </Container>
+  );
 };
 
 export default Error;
